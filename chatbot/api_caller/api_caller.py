65af9f2cc9a2367439b41f22d55b71d697a26fff
mipeso <mikasorvoja1@gmail.com>
2025-10-04 16:45:28 +0300

    Add support for multiple LLM models

--- chatbot/api_caller/api_caller.py
+++ chatbot/api_caller/api_caller.py
@@ -5,27 +5,23 @@ from pathlib import Path
 
 import ollama
 
-MODEL = "deepseek-r1:1.5b"
-
 
 class ROLES(str, Enum):
-    user = ("user",)
+    user = "user"
     system = "system"
 
 
 class APICaller:
-    def __init__(self):
+    def __init__(self, model: str):
         super().__init__()
+        self.model = model
 
     def get_response(self, content: str) -> ollama.ChatResponse:
-        response = ollama.chat(model=MODEL, messages=content, think=False)
+        response = ollama.chat(model=self.model, messages=content, think=False)
         return response
 
     def save_response(self, answer: str, question: str, log_file: Path) -> None:
         answer_cleaned = re.sub(r"<.*?>", "", answer).strip()
-        print(f"Question: {question}")
-        print("Response:")
-        print(answer_cleaned)
         if log_file.exists():
             with log_file.open("r", encoding="utf-8") as f:
                 try:
