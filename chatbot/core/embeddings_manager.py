41995a3199ec65a2d8ce976d20f901a28b0d6231
mipeso <mikasorvoja1@gmail.com>
2026-02-04 09:44:06 +0200

    Refactor main_window.py
    Move methods that are not directly related to GUI to other modules

--- chatbot/core/embeddings_manager.py
+++ chatbot/core/embeddings_manager.py
@@ -1,5 +1,8 @@
 import chromadb
 import ollama
+from PyQt6.QtWidgets import QMainWindow
+
+from chatbot.core.utils import read_file
 
 CLIENT = chromadb.Client()
 COLLECTION = CLIENT.create_collection(name="docs")
@@ -21,7 +24,17 @@ class EmbeddingsManager:
         response = ollama.embed(model=self.model, input=input_message)
 
         query_embedding = response["embeddings"]
-        # Retrieve the most relevant document
+        # Retrieve the most relevant document (n_results=1)
         results = COLLECTION.query(query_embeddings=query_embedding, n_results=1)
 
         return results["documents"][0][0]
+
+    def retrieve_relevant_text(
+        self, main_window: QMainWindow, file_path: str, question: str
+    ) -> str | None:
+        rag_data = read_file(main_window, file_path)
+        self.embed(rag_data)
+        retrieved_text = self.retrieve(question)
+
+        # Return combined retrieved text and user input
+        return "\n".join(retrieved_text) if retrieved_text else None
--- chatbot/core/utils.py
+++ chatbot/core/utils.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 
+import nltk
+from PyQt6.QtWidgets import QFileDialog, QMainWindow, QMessageBox
+
 log_folder = Path(__file__).parent.parent / "logs"
+nltk.download("punkt_tab")
 
 
 def init_log(name: str, model: str, extension: str = "log") -> None:
@@ -23,3 +27,26 @@ def get_logs(model: str) -> list[Path]:
     for file in file_path.glob("*.log"):
         logs.append(file)
     return logs
+
+
+def read_file(main_window: QMainWindow, file_path: str) -> list[str]:
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            text = file.read()
+    except FileNotFoundError:
+        QMessageBox.critical(main_window, "", f"File {file_path} was not found.")
+        return []
+
+    sentences = nltk.sent_tokenize(text)
+
+    return sentences
+
+
+def browse_files(main_window: QMainWindow):
+    dialog = QFileDialog(main_window)
+    file_path, _ = dialog.getOpenFileName(
+        main_window,
+        main_window.tr("Select File"),
+        filter=main_window.tr("Text files (*.txt)"),
+    )
+    main_window.input_file_field.setText(file_path)
